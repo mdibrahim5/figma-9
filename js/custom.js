6c758d59cfb07b57d83dced24db259103a0697a4
Ibrahim5 <104748462+mdibrahim5@users.noreply.github.com>
2024-06-27 16:31:37 +0600

    Add files via upload

---
+++ js/custom.js
@@ -0,0 +1,8 @@
+jQuery (document).ready(function() {
+   jQuery(".owl-carousel").owlCarousel({
+       'items' : 6,
+       'autoPlay' : 2000,
+       'goToFirst': true,
+       'goToFirstSpeed': 1000,
+   });
+});
